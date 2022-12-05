2b387c424b1ee4c76d99b22f3996c73d71dd071e
Jayce Sagvold <97198910+jSagvold28@users.noreply.github.com>
2022-12-05 08:20:04 -0600

    Create TechCV.swift

---
+++ TechCV.swift
@@ -0,0 +1,44 @@
+//
+//  TechCV.swift
+//  Cities.proj
+//
+//  Created by Jayce Sagvold on 12/5/22.
+//
+
+import SwiftUI
+
+struct TechCV: View {
+    var body: some View {
+        NavigationView {
+            List {
+                
+                NavigationLink {
+                    ApplePark()
+                } label: {
+                    Text("Apple Park")
+                }
+                
+                NavigationLink {
+                    TeslaTexas()
+                } label: {
+                    Text("Giga Texas")
+                }
+                
+                NavigationLink {
+                    Microsoft()
+                } label: {
+                    Text("Microsoft")
+                }
+                
+            }
+            .navigationBarTitle("Tech Attractions")
+            .padding(.bottom, 15)
+        }
+    }
+}
+
+struct TechCV_Previews: PreviewProvider {
+    static var previews: some View {
+        TechCV()
+    }
+}
