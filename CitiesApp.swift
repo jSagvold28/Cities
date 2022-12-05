d60abe09bce6d9ade592ee897e9183e9b3fb8d0b
Jayce Sagvold <97198910+jSagvold28@users.noreply.github.com>
2022-12-04 20:59:39 -0600

    Create CitiesApp.swift

---
+++ CitiesApp.swift
@@ -0,0 +1,17 @@
+//
+//  citiesApp.swift
+//  cities
+//
+//  Created by Jayce Sagvold on 12/4/22.
+//
+
+import SwiftUI
+
+@main
+struct citiesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
